1aa748d80f6043ceef1c72cf5122286f53637e55
wudimei <1@wudimei.com>
2021-12-27 10:30:13 +0800

    attr

--- app/index/model/User.php
+++ app/index/model/User.php
@@ -1,17 +1,25 @@
 <?php
 
 namespace app\index\model;
-use  DB\Attributes\Table;
+
+use DB\Attributes\Table;
 use DB\Model;
- 
+use DB\Attributes\PrimaryKey;
+use DB\Attributes\AutoIncrement;
+use DB\Attributes\Column;
 /**
- * more example
+ * more example:
+ * #[Table(name: 'user')]
  * #[Table(name:'user' , connection :'default' )]
  * #[Table(name:'user' , alias:'user', connection :'default' )]
  */
-
-
-#[Table(name:'user'   )]
-class User extends  Model{
-     //pendding :primary key
+ 
+#[Table(name: 'user')]
+class User extends Model {
+    
+    #[PrimaryKey]
+    #[AutoIncrement]
+    #[Column( name: 'id')]
+    protected int $id;
+    
 }
